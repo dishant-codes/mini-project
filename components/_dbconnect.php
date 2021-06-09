609157beff300854ca1cfa6fd62ff0231897e972
Dishant Aarak <57016293+dishant-codes@users.noreply.github.com>
2021-06-09 22:47:27 +0530

    Update _dbconnect.php

--- components/_dbconnect.php
+++ components/_dbconnect.php
@@ -2,10 +2,10 @@
 
 // Script to connect to the database
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "blood_donation";
+$servername = "sql301.epizy.com";
+$username = "epiz_28363607";
+$password = "hz1irYqRE1acNj";
+$database = "epiz_28363607_bloodDonation";
 
 
 $conn = mysqli_connect($servername, $username, $password, $database);
